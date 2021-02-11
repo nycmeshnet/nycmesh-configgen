329d82f375550eab6620159d8a857f9093962f31
olivernyc <oliver@olivernyc.net>
2021-02-10 20:43:05 -0500

    Clean up and fix network number validation

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -3,31 +3,44 @@ import React from "react";
 function Tags({ tags, tagValues, onChange }) {
   if (!tags) return null;
   return tags.map((tag) => {
-    let label = tag
-    let type = "text"
-    let max=""
-    let min=""
-    if (tag === "nodenumber") {
-      label = "Network Number"
-      type="number"
-      max="8000"
-      min="1"
-    }
+    const label = getLabel(tag);
+    const type = getType(tag);
+    const value = tagValues[tag] || "";
+    const handleChange = ({ target }) => {
+      if (tag === "nodenumber") {
+        const isValid = target.value > 0 && target.value <= 8000;
+        if (!isValid) {
+          alert("Network numbers must be between 1 and 8000!");
+          return;
+        }
+      }
+      onChange(tag, target.value);
+    };
+
     return (
       <div key={tag} className="w-100 flex items-center justify-between mt2">
         <label htmlFor={tag}>{label}</label>
         <input
           required
-          spellCheck={false}
           type={type}
-          max={max}
-          min={min}
-          value={tagValues[tag] || ""}
+          value={value}
+          spellCheck={false}
           className="flex w-100 ml3 mw5"
-          onChange={({ target }) => onChange(tag, target.value)}
+          onChange={handleChange}
         />
       </div>
     );
   });
 }
+
+function getLabel(tag) {
+  if (tag === "nodenumber") return "network number";
+  return tag;
+}
+
+function getType(tag) {
+  if (tag === "nodenumber") return "number";
+  return "text";
+}
+
 export default Tags;
