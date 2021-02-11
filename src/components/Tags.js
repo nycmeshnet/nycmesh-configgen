5ba94928bb47dcadc3daf90500ab57b8c481d7fc
olivernyc <oliver@olivernyc.net>
2021-02-10 20:54:40 -0500

    Fix validation

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -4,17 +4,18 @@ function Tags({ tags, tagValues, onChange }) {
   if (!tags) return null;
   return tags.map((tag) => {
     const label = getLabel(tag);
-    const type = getType(tag);
     const value = tagValues[tag] || "";
     const handleChange = ({ target }) => {
+      const { value } = target;
       if (tag === "nodenumber") {
-        const isValid = target.value > 0 && target.value <= 8000;
-        if (!isValid) {
+        const inRange = value > 0 && value <= 8000;
+        const isValid = !isNaN(value) && inRange;
+        if (value && !isValid) {
           alert("Network numbers must be between 1 and 8000!");
           return;
         }
       }
-      onChange(tag, target.value);
+      onChange(tag, value);
     };
 
     return (
@@ -22,7 +23,6 @@ function Tags({ tags, tagValues, onChange }) {
         <label htmlFor={tag}>{label}</label>
         <input
           required
-          type={type}
           value={value}
           spellCheck={false}
           className="flex w-100 ml3 mw5"
@@ -36,11 +36,6 @@ function Tags({ tags, tagValues, onChange }) {
 function getLabel(tag) {
   if (tag === "nodenumber") return "network number";
   return tag;
-}
-
-function getType(tag) {
-  if (tag === "nodenumber") return "number";
-  return "text";
 }
 
 export default Tags;
